3a41b4b5d5964422a8012e30fc7349ac07cd78d2
Alana Grant <2390384g@student.gla.ac.uk>
2021-02-24 10:43:23 +0000

    Update urls.py

--- cs28_project/cs28/urls.py
+++ cs28_project/cs28/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('manage/', views.manage, name='manage'),
-    path('course_grades/', views.module_grades, name='course_grades'),
+    path('course-grades/', views.module_grades, name='course_grades'),
     path('studentUpload/', views.studentUpload, name='studentUpload'),
 ]
