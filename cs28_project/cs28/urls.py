740bfbee4c4fe256f5e7fb674d450e018dc2511d
Nguyen Thanh Hieu <2401707n@student.gla.ac.uk>
2021-03-17 12:25:16 +0100

    Revert "Merge conflicts"
    This reverts commit 9e0a1135522a85fa6c255788030b03dcb12eebba.

--- cs28_project/cs28/urls.py
+++ cs28_project/cs28/urls.py
@@ -25,8 +25,5 @@ urlpatterns = [
     path('module-grades/', views.module_grades, name='module_grades'),
     path('manage/data/', views.data, name='data'),
     path('upload-course-grades/', views.upload_course_grades, name='upload_course_grades'),
-    path('help-page/', views.help, name='help_page'),
-    path('search-results/', views.search_results, name="search_results"),
-    path('graph/', views.graph, name="graph"),
-
+    path('help-page/', views.help, name='help_page')
 ]
